beeecda1723c58866361f71d527707def6c2c20a
PedroL <pepluisgon01@gmail.com>
2023-03-01 21:20:21 -0300

    Adicionando arquivo TXT para usar como estoque e relatorio

--- codigo/Projeto01_LPM/src/application/Program.java
+++ codigo/Projeto01_LPM/src/application/Program.java
@@ -1,5 +1,10 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 import entities.Product;
 import entities.RepositoryOfProducts;
 
@@ -17,6 +22,29 @@ public class Program {
 		rep1.adicionarProduto(p1.getDescricao(), p1.getPrecoCusto());
 		
 		System.out.println(rep1.buscarTodosOsProdutos());
+		
+		
+		
+		
+		// LER DADOS DO RELATORIO
+		File Relatorio = new File("relatorio.txt");
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(Relatorio))) {
+			
+			String produto = br.readLine();
+			while(produto != null) {
+				System.out.println(produto);
+				produto = br.readLine();
+			}
+			
+		} catch(IOException e) {
+			System.out.println("Error reading file: " + e.getMessage());
+		}		
+		
+		System.out.println("==================================================");
+		
+		
+		
 
 	}
 
