cf45226bc675ad90fd48cc6d83052780e68984e5
PedroL <pepluisgon01@gmail.com>
2023-03-08 10:11:19 -0300

    Adicionando infos nos produtos e fazendo o menu

--- codigo/Projeto01_LPM/src/application/Program.java
+++ codigo/Projeto01_LPM/src/application/Program.java
@@ -10,6 +10,7 @@ import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.Scanner;
 
 import entities.Product;
 import entities.RepositoryOfProducts;
@@ -20,6 +21,7 @@ public class Program {
 	public static void main(String[] args) throws ParseException{	
 		
 		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 		
 		RepositoryOfProducts rep1 = new RepositoryOfProducts();
 		
@@ -47,11 +49,70 @@ public class Program {
 		} catch(IOException e) {
 			System.out.println("Error reading file: " + e.getMessage());
 		}		
+		
+		//adicionando atributos nos produtos
+		rep1.buscarProdutoPorId(1).setCustoAquisicao(1000.0);
+		rep1.buscarProdutoPorId(1).setQuantidadeEstoque(15);
+		rep1.buscarProdutoPorId(1).setMargemLucro(25);
+		rep1.buscarProdutoPorId(1).setPrecoCusto(40.0);
+		rep1.buscarProdutoPorId(1).setPrecoVenda(60.0);
+		
+		rep1.buscarProdutoPorId(2).setCustoAquisicao(500.0);
+		rep1.buscarProdutoPorId(2).setQuantidadeEstoque(25);
+		rep1.buscarProdutoPorId(2).setMargemLucro(20);
+		rep1.buscarProdutoPorId(2).setPrecoCusto(10.0);
+		rep1.buscarProdutoPorId(2).setPrecoVenda(20.0);
 
 		
 		System.out.println("================ MERCEARIA =======================");
 		
 		System.out.println();
+		
+		
+		
+		System.out.println("MENU - Escolha uma opção");
+		System.out.println("1 - Buscar produto");
+		System.out.println("2 - Adicionar produto");
+		System.out.println("3 - Atualizar produto");
+		System.out.println("4 - Ver estoque");
+		System.out.println("0 - Sair do programa");
+		int n;
+
+		do {
+		    n = sc.nextInt();
+		    switch (n) {
+		        case 1: 
+		            System.out.print("Informe o id do produto: ");
+		            int id = sc.nextInt();
+		            System.out.println(rep1.buscarProdutoPorId(id));
+		            break;
+		        case 2: 
+		            System.out.print("Informe a descrição do produto: ");
+		            sc.nextLine(); // Consumir a quebra de linha após a leitura do inteiro
+		            String descricao = sc.nextLine();
+		            System.out.print("Informe o id do produto: ");
+		            id = sc.nextInt();
+		            rep1.adicionarProduto(descricao, id);
+		            System.out.println("Produto adicionado");
+		            break;
+		        case 3:
+		            System.out.println("Informe o produto a ser atualizado"); // Corrigir esse metodo de atualizar
+		            rep1.atualizarProduto(null);
+		            break;
+		        case 4:
+		            System.out.println("ESTOQUE");
+		            for (Product p : rep1.buscarTodosOsProdutos()) {
+		                System.out.println(p.toString());
+		            }
+		            break;
+		        case 0:
+		            System.out.println("Encerrando programa...");
+		            break;
+		        default:
+		            System.out.println("Opção inválida. Tente novamente.");
+		    }
+		} while (n != 0);
+
 		
 		// ATUALIZAR O RELATORIO
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter("relatorio.txt"))) {
@@ -72,11 +133,7 @@ public class Program {
 		
 		System.out.println("==================================================");
 		
-		rep1.buscarProdutoPorId(2).setCustoAquisicao(500.0);
-		rep1.buscarProdutoPorId(2).setQuantidadeEstoque(25);
-		rep1.buscarProdutoPorId(2).setMargemLucro(20);
-		rep1.buscarProdutoPorId(2).setPrecoCusto(10.0);
-		rep1.buscarProdutoPorId(2).setPrecoVenda(20.0);
+	
 		
 		System.out.println(rep1.totalDeProdutosNoEstoque());
 		System.out.println(rep1.valorTotaldoEstoque());
